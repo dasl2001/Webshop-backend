8f788eb3c310e89041d540e5f404ecca3f83e060
dasl2001 <david.slivo2001@gmail.com>
2025-03-27 10:25:18 +0100

    add cors dependancy (JV)

--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
-// server.js
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from 'cors';
 import productRoutes from "./routes/products.js";
 import categoryRoutes from "./routes/categories.js";
 
 dotenv.config();
 
 const app = express();
+app.use(cors('*'));
 app.use(express.json());
 
-// Använd dina routes
+//Routes
 app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
 
@@ -22,11 +23,11 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("MongoDB connected");
+    console.log("MongoDB ansluten");
     app.listen(PORT, () =>
       console.log(`Servern körs på http://localhost:${PORT}`)
     );
   })
   .catch((err) => {
-    console.error("Error connecting to MongoDB", err);
+    console.error("Fel vid anslutning till MongoDB", err);
   });
