62f2bda441e518eb59db1f5eee06d4c2db52c3aa
dasl2001 <david.slivo2001@gmail.com>
2025-04-07 20:17:00 +0200

    test

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,13 +1,2 @@
-import mongoose from "mongoose";
 
-const orderSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    address: { type: String, required: true },
-    phone: { type: String, required: true },
-    total: { type: Number, required: true },
-  },
-  { timestamps: true }
-);
-
-export default mongoose.model("Order", orderSchema);
+  
