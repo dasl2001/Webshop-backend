ba371538394fc518e88d3b72f9ce59569cb85377
dasl2001 <70586727+dasl2001@users.noreply.github.com>
2025-05-23 17:34:36 +0200

    first commit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,3 @@
-/*
-Importering av filer om mudler
-*/
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -9,46 +6,20 @@ import productRoutes from "./routes/products.js";
 import categoryRoutes from "./routes/categories.js";
 import authRoutes from "./routes/auth.js";
 import orderRoutes from "./routes/orders.js";
-
-/*
-Initiera appen och läs .env filen
-*/
 dotenv.config();
 const app = express();
-
-/*
-CORS tillåter  frontend att anropa backend.
-*/
 app.use(cors('*'));
 app.use(cors({
     origin: ["http://127.0.0.1:5500", "http://localhost:5500", "https://webshop-2025-fe.vercel.app"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }));
-
-/*
-Automatiskt parsar inkommande JSON
-*/
 app.use(express.json());
-
-/*
-Routing
-*/
 app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/orders", orderRoutes);
-
-/*
-Läser porten från .env, annars standard 3000
-*/
 const PORT = process.env.PORT || 3000;
-
-/*
-Här ansluter du till databasen (via URI i .env)
-När databasen är ansluten → startas servern
-Om något går fel loggas det i konsolen
-*/
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
