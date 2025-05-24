a7df7bdf73936ed04fe170b989b6feb00c43b6f6
dasl2001 <70586727+dasl2001@users.noreply.github.com>
2025-05-24 14:22:43 +0200

    last commit

--- src/__tests__/product.test.js
+++ src/__tests__/product.test.js
@@ -4,7 +4,7 @@ import Product from "../models/Product.js";
 import Category from "../models/Category.js";
 jest.setTimeout(10000); 
 let mongo;
-beforeAll(async () => {
+beforeAll(async () => { 
   mongo = await MongoMemoryServer.create();
   await mongoose.connect(mongo.getUri(), {
     useNewUrlParser: true,
